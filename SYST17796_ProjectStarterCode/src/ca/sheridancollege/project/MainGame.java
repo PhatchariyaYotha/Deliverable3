ffe3324b38f68402fbf3fa54e2f0ecf523bf9e12
Phatchariya Yotha <yotha@sheridancollege.ca>
2023-07-03 10:27:43 -0400

    Create main class

---
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/MainGame.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author phatchariyaaa
+ */
+public class MainGame {
+    public static void main(String[] args){
+        
+        Game warGame = new Game(){
+            @Override
+            public void play(){
+                
+                
+                
+        }
+    };
+    }
+}
+    
+

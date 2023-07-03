5f7b49e475e972b2f691f441ca5f6522437736ca
Phatchariya Yotha <yotha@sheridancollege.ca>
2023-07-02 20:04:40 -0400

    update Card class

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
@@ -12,14 +12,58 @@ package ca.sheridancollege.project;
  * @author dancye
  */
 public abstract class Card {
-    //default modifier for child classes
-
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    private final Rank rank;
+    private final Suit suit;
+    
+    public Card(Rank rank, Suit suit){
+        this.rank=rank;
+        this.suit= suit;
+       
+    }
+    
+    public Rank getRank(){
+        return rank;
+    }
+    
+    public Suit getSuit(){
+        return suit;
+    }
+    
     @Override
-    public abstract String toString();
+    public String toString(){
+        return rank + "of" +suit;
+    }
+    
+    public enum Rank{
+        ACE(1),
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(11),
+        QUEEN(12),
+        KING(13);
+        
+        private final int value;
+        
+        Rank(int value){
+        this.value=value;
 
+    }
+        public int getValue(){
+            return value;
+        }
+}
+
+        public enum Suit{
+            CLUBS,
+            DIAMONDS,
+            HEARTS,
+            SPADES,
+        }
 }

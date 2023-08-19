6d519813dc49b77f5fdff6ed64b21ad9fe34a2c2
Phatchariya Yotha <yotha@sheridancollege.ca>
2023-08-19 15:10:30 -0400

    update Main.java code

--- Deliverable3/src/main/java/com/mycompany/deliverable3/Main.java
+++ Deliverable3/src/main/java/com/mycompany/deliverable3/Main.java
@@ -9,5 +9,18 @@ package com.mycompany.deliverable3;
  * @author phatchariyaaa
  */
 public class Main {
+    public static void main(String[]args){
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        
+        ArrayList<Player>players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        
+        Game game = new Game("War Card Game", players);
+        game.play();
+
+    }
+    
     
 }

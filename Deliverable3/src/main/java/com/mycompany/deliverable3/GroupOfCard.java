879979d8ba93d76cd43a9f3a2211e40825c49273
Phatchariya Yotha <yotha@sheridancollege.ca>
2023-08-19 15:20:21 -0400

    update GroupOfCard.java code

--- Deliverable3/src/main/java/com/mycompany/deliverable3/GroupOfCard.java
+++ Deliverable3/src/main/java/com/mycompany/deliverable3/GroupOfCard.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.deliverable3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author phatchariyaaa
  */
 public class GroupOfCard {
+    private final ArrayList<Card>cards;
     
+    public GroupOfCard(){
+        this.cards = new ArrayList<>();
+        
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Card> getCards(){
+            return cards;
+        }
+    
+    public void addCard(Card card){
+        cards.add(card);
+    }
+    
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
 }
+    
+

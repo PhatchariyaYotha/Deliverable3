a17b20ef13aad27ab5022b3b28b78414d13d137f
Phatchariya Yotha <yotha@sheridancollege.ca>
2023-08-19 15:32:14 -0400

    update enum Rank code

--- Deliverable3/src/main/java/com/mycompany/deliverable3/Card.java
+++ Deliverable3/src/main/java/com/mycompany/deliverable3/Card.java
@@ -9,5 +9,39 @@ package com.mycompany.deliverable3;
  * @author phatchariyaaa
  */
 public class Card {
+    private Rank rank;
+    
+    public Card(Rank rank){
+        this.rank = rank;
+    }
+    
+    
+}
+
+enum Rank{
+    ACE(1), 
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+    
+    private final int value;
+    
+    Rank(int value){
+        this.value =value;
+    }
+    
+    public int getValue(){
+        return value;
+    }
+    
     
 }
